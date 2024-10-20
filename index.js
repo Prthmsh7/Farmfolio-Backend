9dd9edd98dd33dc072677fa49f18ff57361b7e8f
Prthmsh7 <Prathmeshshukla2106@gmail.com>
2024-10-21 01:02:48 +0530

    updated api

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const cors = require("cors");
 
 dbConnect();
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
